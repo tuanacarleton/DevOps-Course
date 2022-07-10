54aa71af49f702256a5b1668a2abf2a22257ab0a
Chris Tuana Carleton <ctuanacarleton@gmail.com>
2022-07-10 16:41:20 -0700

    terraform deployment of jenkins

---
+++ projects/terraform/main.tf
@@ -0,0 +1,74 @@
+provider "aws" {
+  region  = "us-east-1"
+}
+
+# Security Group
+variable "ingressrules" {
+  type    = list(number)
+  default = [8080, 22]
+}
+resource "aws_security_group" "web_traffic" {
+  name        = "Allow web traffic"
+  description = "inbound ports for ssh and standard http and everything outbound"
+  dynamic "ingress" {iterator = port
+    for_each = var.ingressrules
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "TCP"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    "Terraform" = "true"
+  }
+}
+
+# Data Block
+data "aws_ami" "redhat" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["RHEL-7.5_HVM_GA*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["309956199498"]
+}
+
+# resource block
+resource "aws_instance" "jenkins" {
+  ami             = data.aws_ami.redhat.id
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.web_traffic.name]
+  key_name        = "aws_lab_key"
+  
+  provisioner "remote-exec"  {
+    inline  = [
+      "sudo yum install -y jenkins java-11-openjdk-devel",
+      "sudo yum -y install wget",
+      "sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo",
+      "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key",
+      "sudo yum upgrade -y",
+      "sudo yum install jenkins -y",
+      "sudo systemctl start jenkins",
+      ]
+   }
+ connection {
+    type         = "ssh"
+    host         = self.public_ip
+    user         = "ec2-user"
+    private_key  = file(".\aws_lab_key.pem" )
+   }
+  tags  = {
+    "Name"      = "Jenkins"
+      }
+ }
